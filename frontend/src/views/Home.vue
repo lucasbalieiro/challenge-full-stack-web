af79f6099e2bfb4a9465bc6a4f5380cec9f2c7c2
Lucas Balieiro <lucasbalieirom@hotmail.com>
2020-11-23 04:57:08 -0400

    #frontend: added Home View

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,21 +1,7 @@
 <template>
   <div class="home">
     <div class="box">
-      <h1>Bem vindo!</h1>
+      <h1>Desafio Grupo A - EdTech</h1>
     </div>
   </div>
 </template>
-
-<style>
-.home {
-  @media 1260 {
-    width: 100vw;
-  }
-  width: 80vw;
-  height: 80vh;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
-</style>
